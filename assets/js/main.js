c3b575c215177a0401ffe7e7cac4db025a558ed0
Mais <59611735+itsmais@users.noreply.github.com>
2021-02-13 17:20:46 +0200

    added music

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,34 @@
 document.getElementById("start").addEventListener("click", startTimer);
+let music = document.getElementById("alarmMusic");
 
 let isTimeGoingOn = false;
 let minutes;
 let seconds;
 
-
 function startTimer() {
-  if (!isTimeGoingOn){
+  if (!isTimeGoingOn) {
     document.getElementById("start").innerHTML = "Restart";
+    music.pause();
     minutes = 9;
     seconds = 59;
     isTimeGoingOn = true;
     document.getElementById("subtitle").innerHTML = "";
-        
-      setInterval(function () {
-        document.getElementById("countdown").innerHTML = addZeroBefore(minutes) + ":" 
-        + addZeroBefore(seconds);
-        seconds--;
-        if (seconds < 0){
-          seconds = 59;
-          minutes--;
-        }
-        if (minutes < 0){
-          isTimeGoingOn = false;
-          document.getElementById("countdown").innerHTML = "WAKE UP";
-        }
+    setInterval(function () {
+      document.getElementById("countdown").innerHTML = addZeroBefore(minutes) + ":" +
+        addZeroBefore(seconds);
+      seconds--;
+      if (seconds < 0) {
+        seconds = 59;
+        minutes--;
+      }
+      if (minutes < 0) {
+        isTimeGoingOn = false;
+        document.getElementById("countdown").innerHTML = "WAKE UP";
+        // play alarm music
+        music.play();
+      }
     }, 1000);
-  }
-  else{
+  } else {
     minutes = 10;
     seconds = 0;
   }
